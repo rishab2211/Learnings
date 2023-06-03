35e7dcbfa80414ab776b87c2066fd29b4eb2cc18
Rishab2211 <Rishabraj2211@gmail.com>
2023-06-03 22:50:05 +0530

    CODE TO PRINT THE NUMBERS FROM 0 TO n

---
+++ print_num.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class print_num {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter till when you want to print the numbers ");
+        int n = sc.nextInt();
+        System.out.println("Here you Go !");
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
