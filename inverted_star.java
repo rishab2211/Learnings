501e9ce9b6884f61c867137e33904b9f85d119a2
Rishab2211 <Rishabraj2211@gmail.com>
2023-06-04 02:00:19 +0530

    CODE TO PRINT INVERTED STAR PATTERN

--- inverted_star.java
+++ inverted_star.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class inverted_star {
-    public static void main(String args{
-        
-    })
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter how big inverted star pattern you want to make : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 
 }
