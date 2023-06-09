59f1afee50d9a26793ad6bbf4e8314813a9e1cd0
Rishab2211 <Rishabraj2211@gmail.com>
2023-06-09 13:59:01 +0530

    CODE TO PRINT PRIMES IN A RANGE OF 2 to n

---
+++ prime_in_range.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class prime_in_range {
+    public static boolean prime(int n) {
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void inrange(int n1) {
+        for (int i = 2; i <= n1; i++) {
+            if (prime(i)) {
+                System.out.print(i + " ");
+            }
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the last limit of the range : ");
+        int n1 = sc.nextInt();
+        inrange(n1);
+
+    }
+
+}
