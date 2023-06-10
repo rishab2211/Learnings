a71411184a6e60f14a8676c02aac2f1d1828c4be
Rishab2211 <Rishabraj2211@gmail.com>
2023-06-11 03:54:08 +0530

    CODE OF LINEAR SEARCH

---
+++ linear_search.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class linear_search {
+    public static void lin_search(int arr[], int key) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] == key) {
+                System.out.print("the key " + key + " found at " + i + "th position");
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = { 12, 92, 63, 49, 50, 62, 73, 86, 9, 99 };
+        System.out.print("enter the key : ");
+        int key = sc.nextInt();
+        lin_search(arr, key);
+    }
+
+}
