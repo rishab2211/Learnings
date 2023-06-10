a12ad70461a9a587c763f8be1e91d29ddda577bd
Rishab2211 <Rishabraj2211@gmail.com>
2023-06-11 03:53:00 +0530

    CODE TO PRINT DIAMOND PATTERN

---
+++ diamond_patt.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class diamond_patt {
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = n - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 2 * i - 1; k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("how big diamond pattern you want to make ? : ");
+        int n = sc.nextInt();
+        pattern(n);
+    }
+
+}
