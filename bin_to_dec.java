31f0bbdb0e49bd55d200b16765585e3bfe2ad533
Rishab2211 <Rishabraj2211@gmail.com>
2023-06-11 03:32:04 +0530

    CODE TO CONVERT BINARY TO DECIMAL

---
+++ bin_to_dec.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class bin_to_dec {
+    public static void bintodec(int n) {
+        int pow = 0;
+        int dec = 0;
+        while (n > 0) {
+            int ld = n % 10;
+            dec = dec + ld * (int) Math.pow(2, pow);
+            pow++;
+            n = n / 10;
+
+        }
+        System.out.print(dec);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the value of binary to be converted : ");
+        int n = sc.nextInt();
+        bintodec(n);
+    }
+
+}
