a6a12c4caec30152ba7f582a9fd31e8303afe132
Rishab2211 <Rishabraj2211@gmail.com>
2023-06-11 03:50:46 +0530

    CODE TO PRINT SOLID RHOMBUS PATTERN

---
+++ solid_rhombus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class solid_rhombus {
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= n; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("how big rhombus pattern you want ton make ? ");
+        int n = sc.nextInt();
+        pattern(n);
+    }
+
+}
