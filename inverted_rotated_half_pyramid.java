256968a605a6b6a8bdbab71496091dd86355a897
Rishab2211 <Rishabraj2211@gmail.com>
2023-06-11 03:45:10 +0530

    CODE TO PRINT INVERTED AND ROTATED HALF PYRAMID PATTERN

---
+++ inverted_rotated_half_pyramid.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class inverted_rotated_half_pyramid {
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int spaces = 1; spaces <= n - i; spaces++) {
+                System.out.print(" ");
+            }
+            for (int star = 1; star <= i; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("how big inverted half pyramid pattern you want to see ?");
+        int n = sc.nextInt();
+        pattern(n);
+    }
+
+}
