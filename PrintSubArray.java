34506cd03fc64b28a5b52c6d249cdb8fd427c2b6
Rishab2211 <Rishabraj2211@gmail.com>
2023-07-22 19:18:30 +0530

    CODE TO PRINT SUB-ARRAYS OF ANY ARRAY

---
+++ PrintSubArray.java
@@ -0,0 +1,18 @@
+public class PrintSubArray {
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        print(arr);
+    }
+
+}
