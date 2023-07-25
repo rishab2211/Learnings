0257980e8b02ad0a8ebc0eb1c20ad5b176aed1e1
Rishab2211 <Rishabraj2211@gmail.com>
2023-07-26 01:59:07 +0530

    BINARY SEARCH AND MODIFIED BINARY SEARCH CODE

--- binary_search.java
+++ binary_search.java
@@ -3,7 +3,24 @@ public class binary_search {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
-            int mid = (start + end) / 2;
+            int mid = (start + end) / 2;// with this method of calculating mid value INTEGER OVERFLOW can happen
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else if (arr[mid] > key) {
+                end = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+
+    public static int Modified_binary_search(int arr[], int key) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;// with this INTEGER OVERFLOW CAN BE PREVENTED
             if (arr[mid] == key) {
                 return mid;
             } else if (arr[mid] < key) {
@@ -19,7 +36,9 @@ public class binary_search {
     public static void main(String args[]) {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         int key = 9;
-        System.out.print("the index of key is : " + bin_search(arr, key));
+        System.out.println("the index of key is : " + bin_search(arr, key));
+        int key1 = 7;
+        System.out.print("the index of key is : " + Modified_binary_search(arr, key1));
 
     }
 
