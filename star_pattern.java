c97d9de026498a2fdd32f1705330f87e335a41d8
Rishab2211 <Rishabraj2211@gmail.com>
2023-07-27 02:16:04 +0530

    PRINTING STAR PATTERN

--- star_pattern.java
+++ star_pattern.java
@@ -5,6 +5,7 @@ public class star_pattern {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter how big star pattern you want to make : ");
         int n = sc.nextInt();
+        sc.close();// used to resolve source leak problem in system
         for (int i = 1; i <= n; i++) {// outer loop used for the row
             for (int j = 1; j <= i; j++) {// inner loop for the stars
                 System.out.print("*");
