5be136d9e49eba1493bc7f96bac9956a1798136e
Rishab2211 <Rishabraj2211@gmail.com>
2023-07-27 14:56:59 +0530

    PRINTING NUMBERS TILL n

--- print_num.java
+++ print_num.java
@@ -5,6 +5,7 @@ public class print_num {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter till when you want to print the numbers ");
         int n = sc.nextInt();
+        sc.close();// used to resolve source leak problem in system
         System.out.println("Here you Go !");
         for (int i = 0; i <= n; i++) {
             System.out.println(i);
